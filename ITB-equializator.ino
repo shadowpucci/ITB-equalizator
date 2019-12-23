0ea99173fbdb78998893d36409e06a0ac65afa70
shadowpucci <48486558+shadowpucci@users.noreply.github.com>
2019-12-23 16:29:17 -0300

    first commit

---
+++ ITB-equializator.ino
@@ -0,0 +1,96 @@
+/*
+ * ESP32 AJAX Demo
+ * Updates and Gets data from webpage without page refresh
+ * https://circuits4you.com
+ */
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#define led 2
+//sensores
+ 
+#include "index.h"  //Web page header file
+int ADC[] = {36, 39, 34, 35, 32, 33}; // pines ADC de los sensores MAP
+
+ 
+WebServer server(80);
+ 
+//Enter your SSID and PASSWORD
+const char* ssid = "Sir Pepe V";
+const char* password = "fasopucci";
+ 
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+ 
+void handleADC() {
+ int sens[6];
+ int a;
+ digitalWrite(led, HIGH); 
+ String responseText="";
+ for(a=0; a<6; a++){
+    sens[a]= analogRead(ADC[a]);
+    responseText += String(sens[a]);
+    responseText += "#";
+ }
+ Serial.println(responseText);
+ String adcValue = String(sens[2]);
+ 
+ server.send(200, "text/plane", responseText); //Send ADC value only to client ajax request
+ digitalWrite(led, LOW); 
+}
+ 
+//===============================================================
+// Setup
+//===============================================================
+ 
+void setup(void){
+  pinMode(led, OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+ 
+/*
+//ESP32 As access point
+  WiFi.mode(WIFI_AP); //Access Point mode
+  WiFi.softAP(ssid, password);
+*/
+//ESP32 connects to your wifi -----------------------------------
+  WiFi.mode(WIFI_STA); //Connectto your wifi
+  WiFi.begin(ssid, password);
+ 
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
+ 
+  //Wait for WiFi to connect
+  while(WiFi.waitForConnectResult() != WL_CONNECTED){      
+      Serial.print(".");
+    }
+    
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+//----------------------------------------------------------------
+ 
+  server.on("/", handleRoot);      //This is display page
+  server.on("/readADC", handleADC);//To get update of ADC Value only
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+ 
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void loop(void){
+  server.handleClient();
+  delay(1);
+}
+
---
+++ index.h
@@ -0,0 +1,73 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<style>
+.card{
+    max-width: 400px;
+     min-height: 250px;
+     background: #02b875;
+     padding: 30px;
+     box-sizing: border-box;
+     color: #FFF;
+     margin:20px;
+     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);
+}
+</style>
+<body>
+<img src="https://lh3.googleusercontent.com/tE9sHWsSeubGOoRDE4QsbRA4Y1AZvvKa3waWXiFvlRMDAbVSSTDmN5gJPbWbmdS1BwNDatoLaMyjyw2WdsM8PftJ22YxUJdfGHAb9M10iz-0xt1IPJtmJOaZLtjHek5fdUMT4uwuQIoGttTJjbbiob551avofwjriynGNTcMRIp46qtE-WaGqi6Y5wzlzqpQFx1XVppQo4Uv_1PYVMh2CsDc6OZurE6w52kyy7ZhIgx82_mzgOpyqxFPeKf__D-f-QicsgR6X-c8qN8kbxFsGSN5WU93FVxJH5cxAjOmq3UtqKPzomNjvmhKYhLK7B_HL1nc21v7QbPpiK618BYWWI2JROVGoeGcUbE463nWfsXVzzQaAYh9fVGmrRb5XxC6t-x-VY98Zvrlv16oE4LGPbGDgvLGbxntEp3hSBqHF1KLqdMghVkXrSWrtbhZuJvaDuWACnMZmQpKP2zmuZFdWNQ0xG-o-7VicHZArxfEIWr4f52-z-qC3E6J3PbUMonqhesw-TgwpTXLtmZ84G-bTCegzsm5FVd1CGkgStqGaMBKZ4OuCpY4olbEQq6rplnye5jz5_uO7Dt_s0VmujpBEuM7_tJXiU7nxNeUqzpjcVgG-ghgplT29t4N05L9X3ZA7Oi5glHTGAvH9jsLWAxB1-LfT3KuvQu5BIoAY7hyVjXx8osH475hqA=w399-h84-no">
+<br>
+<div class="card">
+  <h1>Six-MAP ITB TUNER</h1><br>
+  <h4>Cyl #1: <meter id="ADCBar0" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue0">0</span></h4> <br>
+  <h4>Cyl #2: <meter id="ADCBar1" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue1">0</span></h4>  <br>
+  <h4>Cyl #3: <meter id="ADCBar2" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue2">0</span></h4> <br>
+  <h4>Cyl #4: <meter id="ADCBar3" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue3">0</span></h4> <br>
+  <h4>Cyl #5: <meter id="ADCBar4" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue4">0</span></h4> <br>
+  <h4>Cyl #6: <meter id="ADCBar5" min="0" max="1100"
+         low="200" high="900"
+         optimum="850" value="300"> </meter>   <span id="ADCValue5">0</span></h4> <br>
+</div>
+<script>
+
+setInterval(function() {
+  // Call a function repetatively with 0.2 Second interval
+  getData();
+}, 200); //2000mSeconds update rate
+
+function getData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response=this.responseText; // Respuesta del ESP con la info
+      var datos=response.split("#");      //Respuesta parseada para separar los datos
+      document.getElementById("ADCValue0").innerHTML = datos[0];
+      document.getElementById("ADCBar0").value = datos[0];
+      document.getElementById("ADCValue1").innerHTML = datos[1];
+      document.getElementById("ADCBar1").value = datos[1];
+      document.getElementById("ADCValue2").innerHTML = datos[2];
+      document.getElementById("ADCBar2").value = datos[2];
+      document.getElementById("ADCValue3").innerHTML = datos[3];
+      document.getElementById("ADCBar3").value = datos[3];
+      document.getElementById("ADCValue4").innerHTML = datos[4];
+      document.getElementById("ADCBar4").value = datos[4];
+      document.getElementById("ADCValue5").innerHTML = datos[5];
+      document.getElementById("ADCBar5").value = datos[5];
+    }
+  };
+  xhttp.open("GET", "readADC", true);
+  xhttp.send();
+}
+</script>
+</body>
+</html>
+)=====";
